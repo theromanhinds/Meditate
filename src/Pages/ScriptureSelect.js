d15aa05ee2a3aff8c2b5910871b9e3a78979e8f2
iamrch <theromanhinds17@gmail.com>
2024-12-31 11:47:12 -0500

    missing dep

--- src/Pages/ScriptureSelect.js
+++ src/Pages/ScriptureSelect.js
@@ -38,7 +38,7 @@ function ScriptureSelect({ setSelectingScripture, requestScripture }) {
           requestScripture(selectedBook, selectedChapter, selectedStartVerse, selectedEndVerse);
           setSelectingScripture(false); // or any other finalization action
         }
-      }, [selectStage, selectedEndVerse, selectedChapter, selectedStartVerse, requestScripture, setSelectingScripture]);
+      }, [selectStage, selectedEndVerse, selectedBook, selectedChapter, selectedStartVerse, requestScripture, setSelectingScripture]);
 
   return (
         <>
